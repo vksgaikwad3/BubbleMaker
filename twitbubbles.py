ad209e5363a1f636b711a00efdcb8a7a41e0ac54
Vikas Gaikwad <vksgaikwad3@gmail.com>
2015-09-26 14:43:38 +0530

    Added python script and Arduino sketch

---
+++ twitbubbles.py
@@ -0,0 +1,40 @@
+import time
+import RPi.GPIO as GPIO
+import serial
+from twython import TwythonStreamer
+
+# Search terms
+TERMS = '#makermela'
+ser = serial.Serial('/dev/ttyACM0', 9600)
+# GPIO pin number of LED
+LED = 22
+
+# Twitter application authentication
+# replace with your credentials
+APP_KEY = ''
+APP_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
+
+# Setup callbacks from Twython Streamer
+class BlinkyStreamer(TwythonStreamer):
+        def on_success(self, data):
+                if 'text' in data:
+                        print data['text'].encode('utf-8')
+ 			print '-'
+			ser.write('a')
+			
+
+# Setup GPIO as output
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.output(LED, GPIO.LOW)
+
+# Create streamer
+try:
+        stream = BlinkyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+        stream.statuses.filter(track=TERMS)
+except KeyboardInterrupt:
+        GPIO.cleanup()
+
+
